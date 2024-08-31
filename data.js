a0808647dd61d9b68440bd4958dcc47a7af3e9c9
NiiloOikari <Niilo.Oikari@gmail.com>
2024-08-31 13:37:40 +0300

    Lisätty tiedostot

---
+++ data.js
@@ -0,0 +1,455 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": 0.04698904183841002,
+        "pitch": 0.06286453730084673,
+        "fov": 1.5308968041190865
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.139181456996136,
+          "pitch": 0.45770808575114863,
+          "rotation": 0,
+          "target": "1-25"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.9591346719068472,
+          "pitch": -0.525753939490798,
+          "title": "Yöpöllö",
+          "text": ""
+        },
+        {
+          "yaw": 2.4797348732386144,
+          "pitch": 0.4153625462830437,
+          "title": "Hiiret avaruussäässä",
+          "text": "<div><br></div>"
+        },
+        {
+          "yaw": -0.07730070188920024,
+          "pitch": 0.44289563441255453,
+          "title": "Piian hetki",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "1-25",
+      "name": "2.5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.33988215775337594,
+          "pitch": 0.29687472461778164,
+          "rotation": 0,
+          "target": "0-1"
+        },
+        {
+          "yaw": 0.6081795318369778,
+          "pitch": 0.5226934466803677,
+          "rotation": 5.497787143782138,
+          "target": "3-3"
+        },
+        {
+          "yaw": 1.5995219418256967,
+          "pitch": 0.4722024945288368,
+          "rotation": 0.7853981633974483,
+          "target": "4-4"
+        },
+        {
+          "yaw": -2.137292657840927,
+          "pitch": 0.4497160184670186,
+          "rotation": 5.497787143782138,
+          "target": "6-6"
+        },
+        {
+          "yaw": -1.4304205337612608,
+          "pitch": 0.42624136464464435,
+          "rotation": 0.7853981633974483,
+          "target": "5-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-3",
+      "name": "3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": 0.2817460078219529,
+        "pitch": 0.16398167685495402,
+        "fov": 1.5308968041190865
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.039019874506362,
+          "pitch": 0.5203414318886139,
+          "rotation": 5.497787143782138,
+          "target": "4-4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.3088393537561558,
+          "pitch": 0.27889391867551794,
+          "title": "Alkusoitto",
+          "text": ""
+        },
+        {
+          "yaw": 0.5192593420271514,
+          "pitch": 0.3463317168335216,
+          "title": "Taivaan merkit",
+          "text": ""
+        },
+        {
+          "yaw": 1.3276760553438738,
+          "pitch": 0.2386007729665991,
+          "title": "Kosminen huilusoolo",
+          "text": ""
+        },
+        {
+          "yaw": 1.803498950264295,
+          "pitch": 0.24680329196821127,
+          "title": "Karhu ja taivas",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "4-4",
+      "name": "4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": 2.5032203829409987,
+        "pitch": 0.06188867046971325,
+        "fov": 1.5308968041190865
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4824197882468848,
+          "pitch": 0.5453005921542697,
+          "rotation": 0,
+          "target": "3-3"
+        },
+        {
+          "yaw": -0.6441106554002403,
+          "pitch": 0.44899402364877616,
+          "rotation": 0,
+          "target": "1-25"
+        },
+        {
+          "yaw": -1.0752232073903407,
+          "pitch": 0.2770818980614944,
+          "rotation": 5.497787143782138,
+          "target": "6-6"
+        },
+        {
+          "yaw": -0.5807722571650693,
+          "pitch": 0.23275645021789515,
+          "rotation": 0.7853981633974483,
+          "target": "5-5"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.744448431062116,
+          "pitch": 0.39164485022100237,
+          "title": "Peltojen Coltrane",
+          "text": ""
+        },
+        {
+          "yaw": 2.5985270754702325,
+          "pitch": 0.45102647865653367,
+          "title": "Like Sonny",
+          "text": ""
+        },
+        {
+          "yaw": -2.9282181711467423,
+          "pitch": 0.47093739730483186,
+          "title": "Sellisti pellossa",
+          "text": ""
+        },
+        {
+          "yaw": -2.153027149351608,
+          "pitch": 0.4534227540054907,
+          "title": "Pelimanni",
+          "text": "https://www.onnelaart.fi/wp-content/uploads/2024/03/Pelimanni-poster-mockup.jpg"
+        }
+      ]
+    },
+    {
+      "id": "5-5",
+      "name": "5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": 2.7195969737325942,
+        "pitch": 0.25213804660116246,
+        "fov": 1.5308968041190865
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.18089943506133466,
+          "pitch": 0.5033430065765607,
+          "rotation": 0,
+          "target": "1-25"
+        },
+        {
+          "yaw": 0.8037770966632447,
+          "pitch": 0.47205185935041705,
+          "rotation": 0.7853981633974483,
+          "target": "6-6"
+        },
+        {
+          "yaw": -0.21802334400400447,
+          "pitch": 0.19036117350024995,
+          "rotation": 0.7853981633974483,
+          "target": "4-4"
+        },
+        {
+          "yaw": -0.7339803586318254,
+          "pitch": 0.21032471134660113,
+          "rotation": 5.497787143782138,
+          "target": "3-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.0672322452237886,
+          "pitch": 0.44764336825856965,
+          "title": "Hip saxofonist",
+          "text": ""
+        },
+        {
+          "yaw": 2.762665053121724,
+          "pitch": 0.44556360938340767,
+          "title": "Montmartre",
+          "text": ""
+        },
+        {
+          "yaw": -2.0533916667941643,
+          "pitch": 0.6177265588913095,
+          "title": "Tyranno sisters",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.682593891995884,
+          "pitch": -0.16501310356446197,
+          "title": "Second location",
+          "text": ""
+        },
+        {
+          "yaw": -1.4837193821491361,
+          "pitch": 0.32224336690180166,
+          "title": "Geometrica",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "6-6",
+      "name": "6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": -2.753498704800423,
+        "pitch": 0.23243734293797225,
+        "fov": 1.5308968041190865
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4989632216990181,
+          "pitch": 0.535663845902242,
+          "rotation": 0,
+          "target": "1-25"
+        },
+        {
+          "yaw": -0.7822752066426393,
+          "pitch": 0.4376829017981976,
+          "rotation": 5.497787143782138,
+          "target": "5-5"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.0339269968887344,
+          "pitch": 0.3120089691160821,
+          "title": "Kaupunki",
+          "text": ""
+        },
+        {
+          "yaw": -2.3125278150133646,
+          "pitch": -0.01266885407195062,
+          "title": "Lady day",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.5823369157214167,
+          "pitch": 0.5071986151340191,
+          "title": "Spirit of Django",
+          "text": ""
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
